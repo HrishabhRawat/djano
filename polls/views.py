d71ed102fa2811fc93e882b61ad449dbcd804838
HrishabhRawat <hrishabhrawat100@gmail.com>
2026-05-21 20:56:10 +0530

    create different views for the application

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World, You are at the poll index")
+    latest_question_list = Question.objects.order_by("pub_date")[:5] 
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You are lookin at the question %s."%question_id)
+
+def results(request, question_id):
+    response = "You are lookin at the result of the Question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on the question %s." %question_id)
+
+
 
