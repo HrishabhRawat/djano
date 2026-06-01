aed44e92e462bd234ab5f6df5eaffac51afb7ab8
HrishabhRawat <hrishabhrawat100@gmail.com>
2026-06-01 15:23:08 +0530

    add login, logout, sinup feature

--- pollproject/settings.py
+++ pollproject/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Where to redirect users after logging in
+LOGIN_REDIRECT_URL = "polls:index"
+
+# Where to redirect users after logging out
+LOGOUT_REDIRECT_URL = "polls:index"
